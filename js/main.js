c18b2321f215dd300104abf8f9801b81c4cfd143
deepjyoti30 <deep.barman30@gmail.com>
2020-05-12 20:56:41 +0530

    Added a message.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ window.onload = function() {
 }
 
 searchBarId = "search-bar-input"
+messageId = "message h2"
+userName = "Deepjyoti"
 
 function initBody() {
     /**
@@ -21,4 +23,47 @@ function initBody() {
         query = document.getElementById(searchBarId).value.replace(/\ /g, "+")
         document.location = googleSearchUrl + query
     })
+    
+    // Build a message for the user
+    builtMsg = buildMsg()
+    builtMsg == "" ? 
+        builtMsg = `Hello ${userName}` : builtMsg = `Hey ${userName}, ${builtMsg}!`
+    $("#" + messageId).text(builtMsg)
+}
+
+function buildMsg() {
+    /**
+     * Build a nice message for the user.
+     * 
+     * Following is how the message would be decided.
+     * 0 - 5:59 : It's too late, take some sleep
+     * 6 - 8:59 : You're up early
+     * 9 - 11:59 : Have a good day ahead
+     * 12 - 16:59 : Good Afternoon
+     * 17 - 19:59 : Good Evening
+     * 20 - 23:59 : Get away from the computer
+     */
+    date = new Date()
+    currentHour = date.getHours()
+    currentMinute = date.getMinutes()
+    currentTime = currentHour + (0.01 * currentMinute)
+
+    if (inRange(currentTime, 0, 5.59))
+        return "It's too late, take some sleep"
+    if (inRange(currentTime, 6, 8.59))
+        return "You're up early"
+    if (inRange(currentTime, 9, 11.59))
+        return "Have a good day ahead"
+    if (inRange(currentTime, 12, 16.59))
+        return "Good Afternoon"
+    if (inRange(currentTime, 17, 19.59))
+        return "Good Evening"
+    if (inRange(currentTime, 20, 24))
+        return "Get away from the computer"
+    else
+        return ""
+}
+
+function inRange(number, min, max) {
+    return (number >= min && number <= max)
 }
