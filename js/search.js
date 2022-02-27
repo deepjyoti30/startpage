167cbff42141fd0df65f71203d56fb963132b89f
Deepjyoti Barman <deep.barman30@gmail.com>
2022-02-27 17:02:28 +0530

    Add init structure for quicklinks config

---
+++ js/search.js
@@ -0,0 +1,13 @@
+/**
+ * Handle quick search functionality to let the user search
+ * various quickly visited sites.
+ * 
+ * We will support things like g'test that will search for `test` on
+ * GitHub.
+ * 
+ * We will also support things like r/r/startpage that will resolve to
+ * reddit.com/r/startpage
+ * 
+ * This functionality is inspired from the tilde projects quick links:
+ * https://github.com/cadejscroggins/tilde 
+ */
